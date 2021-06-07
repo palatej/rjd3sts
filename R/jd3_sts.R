0e165e5a095765f2b93e724eeb4a0ac28bc22ffc
palatej <jean.palate@nbb.be>
2021-06-07 17:29:32 +0200

    STS. Improvements

--- R/jd3_sts.R
+++ R/jd3_sts.R
@@ -2,28 +2,34 @@
 #' @checkmate
 NULL
 
-#' Structural time series
+#' Title
 #'
 #' @param y 
+#' @param X 
 #' @param level 
 #' @param slope 
 #' @param cycle 
 #' @param noise 
 #' @param seasonal 
+#' @param diffuse.regs 
+#' @param tol 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sts<-function(y, X=NULL, level=1, slope=1, cycle=-1, noise=1
-              , seasonal=c("Trigonometric", "Dummy", "Crude", "HarrisonStevens", "Fixed", "Unused"), tol=1e-12){
+std<-rjd3modelling::td(12, c(1992,1), 228, c(1,1,1,1,2,3,0))
+ts<-function(y, X=NULL, level=1, slope=1, cycle=-1, noise=1
+              , seasonal=c("Trigonometric", "Dummy", "Crude", "HarrisonStevens", "Fixed", "Unused"), diffuse.regs=T, tol=1e-9){
   
   if (!is.ts(y)){
     stop("y must be a time series")
   }
   seasonal<-match.arg(seasonal)
-  jsts<-.jcall("demetra/sts/r/Bsm", "Ldemetra/sts/BasicStructuralModel;", "process", ts_r2jd(y), matrix_r2jd(X),
-              as.integer(level), as.integer(slope), as.integer(cycle), as.integer(noise), seasonal, tol)
+  jts<-ts_r2jd(y)
+  jx<-matrix_r2jd(X)
+  jsts<-.jcall("demetra/sts/r/Bsm", "Ldemetra/sts/BasicStructuralModel;", "process", jts, jx,
+              as.integer(level), as.integer(slope), as.integer(cycle), as.integer(noise), seasonal, as.logical(diffuse.regs), tol)
   buffer<-.jcall("demetra/sts/r/Bsm", "[B", "toBuffer", jsts)
   p<-RProtoBuf::read(sts.Bsm, buffer)
   return (p2r_sts_rslts(p))
@@ -60,7 +66,8 @@ p2r_sts_rslts<-function(p){
   
   return (structure(list(
     description=p2r_sts_description(p$description),
-    estimation=p2r_sts_estimation(p$estimation)),
+    estimation=p2r_sts_estimation(p$estimation),
+    decomposition=p2r_sts_components(p$components)),
     class="JD3STS")
   )
 }
@@ -84,6 +91,23 @@ p2r_sts_description<-function(p){
     variables=p2r_variables(p$variables)))
 }
 
+p2r_sts_components<-function(p){
+  return (list(
+    level=p2r_sts_component(p$level),
+    slope=p2r_sts_component(p$slope),
+    cycle=p2r_sts_component(p$cycle),
+    seasonal=p2r_sts_component(p$seasonal),
+    noise=p2r_sts_component(p$noise)
+  ))
+}
+
+p2r_sts_component<-function(p){
+  if (is.null(p)) return (NULL) else return (p$as.list())
+}
+
+
+
+
 p2r_spec_bsm<-function(p){
   return (list(
     level=p2r_parameter(p$level),
@@ -106,22 +130,38 @@ p2r_spec_bsm<-function(p){
 #' @export
 #'
 #' @examples
-print.JDSTS<-function(m){
+print.JD3STS<-function(m){
   cat("Structural time series", "\n\n")
   cat("Variances:\n")
-  s<-m$model$level
-  if (! is.na(s) && s >=0) cat("level: ", format(round(s, 6), scientific = FALSE), "\n")
-  s<-m$model$slope
-  if (! is.na(s) && s >=0) cat("slope: ", format(round(s, 6), scientific = FALSE), "\n")
-  s<-m$model$seas
-  if (! is.na(s) && s >=0) cat("seas: ", format(round(s, 6), scientific = FALSE), "\n")
-  s<-m$model$n
-  if (! is.na(s) && s >=0) cat("noise: ", format(round(s, 6), scientific = FALSE), "\n\n")
-  s<-m$likelihood$ll
-  cat("LogLikelihood: ", format(round(s, 5), scientific = FALSE), "\n")
-  s<-m$estimation$score
-  cat("Scores: ", format(round(s, 5), scientific = FALSE), "\n")
+  s<-m$description$bsm$level
+  if (! is.null(s)) cat("level: ", format(round(s$value, 6), scientific = FALSE), "\n")
+  s<-m$description$bsm$slope
+  if (! is.null(s)) cat("slope: ", format(round(s$value, 6), scientific = FALSE), "\n")
+  s<-m$description$bsm$seas
+  if (! is.null(s)) cat("seasonal: ", format(round(s$value, 6), scientific = FALSE), "\n")
+  s<-m$description$bsm$noise
+  if (! is.null(s)) cat("noise: ", format(round(s$value, 6), scientific = FALSE), "\n\n")
+  s<-m$description$bsm$cycle
+  if (! is.null(s)) {
+    cat("cycle: ", format(round(s$value, 6), scientific = FALSE), "\n\n")
+  }
   
-  #      ll<-proc_numeric(object@internal,"likelihood.ll")
-  #      cat("Log likelihood = ", format(round(ll, 4), scientific = FALSE), "\n")
+  s<-m$estimation$likelihood$ll
+  cat("LogLikelihood: ", format(round(s, 5), scientific = FALSE), "\n")
+  s<-m$estimation$likelihood$aic
+  cat("AIC: ", format(round(s, 5), scientific = FALSE), "\n\n")
+  
+  cat("Regression:\n")
+  regs<-do.call("rbind", lapply(m$description$variables, function(z){z$coeff}))
+  xregs<-cbind(regs, stde=NA, t=NA, pvalue=NA)
+  stde<-sqrt(diag(m$estimation$bvar))
+  sel<-xregs$type=='ESTIMATED'
+  t<-xregs$value[sel]/stde
+  ndf<-m$estimation$likelihood$nobs-m$estimation$likelihood$ndiffuse-m$estimation$likelihood$nparams+1
+  pval<-2*pt(abs(t), ndf, lower.tail = F)
+  xregs$stde[sel]<-stde
+  xregs$t[sel]<-t
+  xregs$pvalue[sel]<-pval
+  print(xregs[-2])
+  
 }
